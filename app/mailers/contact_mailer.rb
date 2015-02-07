33f7845e38332b060fae2b80b1614d924ebf0086
Dietrich <dietricherwane@yahoo.fr>
2015-02-07 19:22:51 +0000

    changes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,6 +5,6 @@ class ContactMailer < ActionMailer::Base
     @contact_info = contact_info
     subject = (contact_info.subject.name rescue "")
 
-    mail(to: "dietricherwane@yahoo.fr", subject: subject)
+    mail(to: ["dietricherwane@yahoo.fr", "likouassi@yahoo.com", "drseka@hotmail.com", "mbinde2006@yahoo.com", "lacitadelle15@yahoo.fr", "hyeboua@yahoo.fr"], subject: subject)
   end
 end
