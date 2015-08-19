f807209da985d1367e042ce9476481c63a7d2f60
Dietrich <dietricherwane@yahoo.fr>
2015-08-19 17:09:57 +0000

    changes

--- app/models/completed_project_image.rb
+++ app/models/completed_project_image.rb
@@ -5,7 +5,7 @@ class CompletedProjectImage < ActiveRecord::Base
   belongs_to :completed_project
 
   # Paperclip
-  has_attached_file :photo, :styles => { :medium => "800x600>", :thumb => "200x200>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :styles => { :medium => "800x600>", :medium_details => "475x300>", :thumb => "200x200>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
